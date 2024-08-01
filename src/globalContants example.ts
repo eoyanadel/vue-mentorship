3074c8999d4f33f2c7f3e1c8db6eb9a94c5e02da
Esteban Oyanadel <esteban.oyanadel@vivint.com>
2024-08-01 17:02:38 -0400

    Removing globalConstants from git

---
+++ src/globalContants example.ts
@@ -0,0 +1,4 @@
+export const BASE_API_URL = 'https://api.themoviedb.org/3/';
+export const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+export const API_KEY = 'YOUR_API_KEY';
+export const MOVIE_API_INJECTION_KEY = 'YOUR_API_INJECTION_KEY';
