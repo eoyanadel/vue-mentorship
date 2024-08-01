a11286de5de0a42bb3c865fdaa68af181fe25598
Esteban Oyanadel Castillo <eoc.ingecomp@gmail.com>
2024-08-01 17:11:27 -0400

    Delete src/globalContants.ts

--- src/globalContants.ts
+++
@@ -1,4 +0,0 @@
-export const BASE_API_URL = 'https://api.themoviedb.org/3/';
-export const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
-export const API_KEY = 'af5fafe744f0cd1965ccad1aaa174540';
-export const MOVIE_API_INJECTION_KEY = 'movie-api-injection-key';
